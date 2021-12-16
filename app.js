5de8fd7b8902aa0878f8d2ea6e2699c4d7d52ee0
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2021-12-15 22:30:25 -0600

    ES6 classes and sub classes

--- app.js
+++ app.js
@@ -85,3 +85,74 @@ Customer.prototype.greeting = function(){
 }
 
 console.log(customer1.greeting());
+
+
+
+// Es6 classes and sub classes and that will be better and easier than Es5
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+mary.getsMarried('Thompson');
+
+console.log(mary);
+
+console.log(Person.addNumbers(1,2));
+
+
+// sub classes with Es6
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Customer extends Person {  // with using extends, you can bring whatever you need from the main class which is person 
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
+  }
+
+  static getMembershipCost() {
+    return 500;
+  }
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
